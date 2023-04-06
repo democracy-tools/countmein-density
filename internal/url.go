f0095538b413248ecace46b0df06a51e1460b8c4
effoeffi <effoeffi@gmail.com>
2023-04-06 20:14:37 +0300

    getAvailablePolygons fix filter

--- internal/url.go
+++ internal/url.go
@@ -3,7 +3,7 @@ package internal
 const (
 	// RegisterUrl = "https://storage.googleapis.com/countmein-web/register.html"
 	VerificationUrl = "https://storage.googleapis.com/countmein-web/phone-verification.html"
-	JoinUrl         = "https://storage.googleapis.com/countmein-web/join.html"
+	JoinUrl         = "https://storage.googleapis.com/countmein-web/join-demonstration.html"
 	ObservationUrl  = "https://storage.googleapis.com/countmein-web/demonstration.html"
 
 	WhatAppGroupLink = "https://chat.whatsapp.com/EtPyiqVEsrLKfuBYJAe26k"
