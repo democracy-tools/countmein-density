cfc7ef98c310b86b8faa3cb85b2854d0d77c70e0
effoeffi <effoeffi@gmail.com>
2023-04-06 18:19:46 +0300

    fix TestHandle_Register

--- internal/register_test.go
+++ internal/register_test.go
@@ -7,7 +7,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"cloud.google.com/go/datastore"
 	"github.com/democracy-tools/countmein-density/internal"
 	"github.com/democracy-tools/countmein-density/internal/ds"
 	whatsapp "github.com/democracy-tools/countmein-density/internal/whatapp"
@@ -23,15 +22,9 @@ func TestHandle_Register(t *testing.T) {
 	}))
 	r, err := http.NewRequest(http.MethodPost, "/register", &buf)
 	require.NoError(t, err)
-
-	dsc := ds.NewInMemoryClient().(*ds.InMemoryClient)
-	dsc.SetGetFilterDelegate(func(kind ds.Kind, filterFieldName string, filterOperator string, filterValue interface{}, dst interface{}) error {
-		return datastore.ErrNoSuchEntity
-	})
-
 	w := httptest.NewRecorder()
 
-	internal.NewHandle(dsc, whatsapp.NewInMemoryClient()).Register(w, r)
+	internal.NewHandle(ds.NewInMemoryClient(), whatsapp.NewInMemoryClient()).Register(w, r)
 
 	require.Equal(t, http.StatusCreated, w.Result().StatusCode)
 }
