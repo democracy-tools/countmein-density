df938ca42235e050c74cf6426adc04658bd13f3e
effoeffi <effoeffi@gmail.com>
2023-04-25 17:29:31 +0300

    Change polygon REST API

--- internal/volunteer.go
+++ internal/volunteer.go
@@ -2,11 +2,13 @@ package internal
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/democracy-tools/countmein-density/internal/ds"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 func (h *Handle) GetVolunteer(w http.ResponseWriter, r *http.Request) {
@@ -41,51 +43,56 @@ func (h *Handle) GetVolunteer(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handle) ChangePolygon(w http.ResponseWriter, r *http.Request) {
 
-	// userId, polygon := mux.Vars(r)["user-id"], mux.Vars(r)["polygon"]
-	// if !validatePolygon(polygon) {
-	// 	logrus.Infof("[ChangePolygon] invalid polygon '%s' user '%s'", polygon, userId)
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
+	userId, newPolygon := mux.Vars(r)["user-id"], mux.Vars(r)["polygon"]
+	if !validateToken(userId) || !validatePolygon(newPolygon) {
+		logrus.Infof("[ChangePolygon] invalid polygon '%s' user '%s'", newPolygon, userId)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	// demonstration, err := ds.GetKaplanDemonstration(h.dsc)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
+	demonstration, err := ds.GetKaplanDemonstration(h.dsc)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// availablePolygons, err := getAvailablePolygons(h.dsc, demonstration.Id)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
+	availablePolygons, err := getAvailablePolygons(h.dsc, demonstration.Id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// if _, ok := availablePolygons[polygon]; !ok {
-	// 	w.Write([]byte(fmt.Sprintf("Polygon %s is not available", polygon)))
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
+	if _, ok := availablePolygons[newPolygon]; !ok {
+		w.Write([]byte(fmt.Sprintf("Polygon %s is not available", newPolygon)))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	// var volunteer ds.Volunteer
-	// err = h.dsc.Get(ds.KindVolunteer, ds.GetVolunteerId(demonstration.Id, userId), &volunteer)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-	// assignedPolygon := volunteer.Polygon
-	// volunteer.Polygon = polygon
-	// err = h.dsc.Put(ds.KindVolunteer, ds.GetVolunteerId(demonstration.Id, userId), &volunteer)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
+	var volunteer ds.Volunteer
+	err = h.dsc.Get(ds.KindVolunteer, ds.GetVolunteerId(demonstration.Id, userId), &volunteer)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	oldPolygon := volunteer.Polygon
+	volunteer.Polygon = newPolygon
+	err = h.dsc.Put(ds.KindVolunteer, ds.GetVolunteerId(demonstration.Id, userId), &volunteer)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// err = h.wac.SendDemonstrationTemplate(user.Phone, demonstration.Id, user.Id, url.QueryEscape(user.Name), polygon, location)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// msg := fmt.Sprintf("Volunteer %s (%s) changed polygon from %s to %s demonstration %s", user.Name, user.Phone, polygon, demonstration.Name, demonstration.Id)
-	// log.Info(msg)
-	// h.sc.Info(msg)
+	var user ds.User
+	err = h.dsc.Get(ds.KindUser, userId, &user)
+	if err != nil {
+		user = ds.User{
+			Id:    userId,
+			Phone: "na",
+			Name:  err.Error(),
+		}
+	}
+	msg := fmt.Sprintf("Volunteer %s (%s) changed polygon from %s to %s demonstration %s",
+		user.Name, user.Phone, oldPolygon, newPolygon, demonstration.Id)
+	log.Info(msg)
+	h.sc.Info(msg)
 }
