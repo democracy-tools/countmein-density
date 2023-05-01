ef8ab64427c20842b956e9884136025809b5c9aa
effoeffi <effoeffi@gmail.com>
2023-05-01 23:06:27 +0300

    split polygons

--- internal/join.go
+++ internal/join.go
@@ -9,8 +9,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var priority = []string{"A5", "A4", "A3", "A13", "A12", "A11", "A7", "A8", "A10", "A9", "A18", "A19",
-	"A20", "A24", "A23", "A14", "A17", "K1", "K2", "K3", "K4", "K5", "K6", "K7", "K9", "K10", "K11",
+var priority = []string{"A5", "A4", "A3", "A13", "A12", "A11", "A7", "A7B", "A8", "A10", "A9", "A18", "A19",
+	"A20", "A24", "A23", "A14A", "A14", "A14B", "A17", "A17B", "K1", "K1A", "K2", "K2B", "K3", "K4", "K5", "K6", "K7", "K9", "K10", "K11",
 	"K12", "K13", "K14", "K19", "K20", "K21", "K22", "K26"}
 
 func (h *Handle) Join(user *ds.User) error {
