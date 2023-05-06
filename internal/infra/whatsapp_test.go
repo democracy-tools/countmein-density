63c5bb3067c744336379926582f025e2d7993775
effoeffi <effoeffi@gmail.com>
2023-05-06 14:22:52 +0300

    [infra] TestSendWhatsAppMessageToNewUsers

--- internal/infra/whatsapp_test.go
+++ internal/infra/whatsapp_test.go
@@ -3,15 +3,31 @@ package infra
 import (
 	"testing"
 
+	"github.com/democracy-tools/countmein-density/internal/ds"
+	"github.com/democracy-tools/countmein-density/internal/env"
 	whatsapp "github.com/democracy-tools/countmein-density/internal/whatsapp"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSendWhatsAppMessage(t *testing.T) {
+func TestSendWhatsAppMessageToNewUsers(t *testing.T) {
 
-	// env.Initialize()
 	t.Skip("infra")
+	// env.Initialize()
+
+	// 6 days ago
+	// fmt.Println(time.Now().Add(time.Hour * 24 * (-6)).Unix())
+
+	dsc := ds.NewClientWrapper(env.Project)
+	var users []ds.User
+	require.NoError(t, dsc.GetFilter(ds.KindUser,
+		[]ds.FilterField{{
+			Name:     "time",
+			Operator: ">",
+			Value:    1682853520,
+		}}, &users))
 
 	wac := whatsapp.NewClientWrapper()
-	require.NoError(t, wac.Send("9721234567", "היי, רצינו לעדכן שניתן לשנות מיקום ע״י הלינק באפליקציה של שינוי פוליגון. נתראה בהפגנה :)"))
+	for _, curr := range users {
+		require.NoError(t, wac.Send(curr.Phone, "היי תודה שהצטרפת אלינו! רצינו לעדכן שניתן לשנות את הפוליגון שלך (האזור שהוקצה לך לדווח ממנו) ע״י הלינק בתחתית העמוד של האפליקציה - שינוי פוליגון. נתראה בהפגנה :)"))
+	}
 }
